4e6a495334935339bc4cc3ecb57fb077a74348fc
garynaz <garynaz@icloud.com>
2021-07-28 20:57:26 -0400

    Initial Commit

---
+++ AWSupportLogger/AWSupportLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWSupportLoggerApp.swift
+//  AWSupportLogger
+//
+//  Created by Gary Naz on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct AWSupportLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AWSupportLogger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AWSupportLogger
+//
+//  Created by Gary Naz on 7/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
